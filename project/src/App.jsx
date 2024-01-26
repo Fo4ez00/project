3935a29833dad4627ca8060107354e43476e5a01
Fo4ez00 <fo4ez1111@gmail.com>
2024-01-26 16:08:09 +0200

    123

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,11 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 
 const App = () => {
-  const handleLogin = (username) => {
-    console.log("Handling login for user:", username);
-  };
-
-  return <LoginPage onLogin={handleLogin} />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
